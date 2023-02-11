ee83dfeed53977e1bcde5b165924d58deaeec806
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-11 20:46:53 +0100

    x

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,32 +10,20 @@ import prisma from "./instance";
 //J&J
 const OVERVIEW__QUERY = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=JNJ&apikey=O6TPBPK501WMPD9J";
 
-var parsedCompany:any = {}
+var parsedCompany:any = {};
+(async ()=>{
+    fetch(OVERVIEW__QUERY).then((data:any)=>{parsedCompany = data.json();
+    console.log('diselo',parsedCompany)
+    })  
+})();
 
 
-request.get({
-    url: OVERVIEW__QUERY,
-    json: true,
-    headers: {'User-Agent': 'request'}
-  }, (err:any, res:any, data:any) => {
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-      // data is successfully parsed as a JSON object:
-      console.log(data);
-      parsedCompany = data;
-    }
-});
-
-
-// insert data into db, resolving all async promises
-// this code can compartimentalize further creating an abstract factory of ".createMany()" functions
-Promise.all([
-    async function () {
-        await prisma.company.create({
-            data: parsedCompany
-        });
-    }()
-]);
+const createNull = prisma.company.create({
+    data: {
+      symbol       : parsedCompany.symbol,
+      assetType    : parsedCompany.assetType,
+      name         : parsedCompany.name,
+      description  : parsedCompany.description
+    },
+  });
+  console.log('hey',createNull)
