e1d7a2218aa895722e014aa35b1bc15fdedabc5f
Josep Abellana <josep.abe.pu@gmail.com>
2023-04-04 21:48:39 +0200

    x

--- server/historical_data.py
+++ server/historical_data.py
@@ -14,7 +14,6 @@ print(data.tail())
 import yfinance as yf
 start_date = '2020-01-01'
 end_date = '2022-01-01'
- 
 # Add multiple space separated tickers here
 ticker = 'GOOGL MSFT TSLA'
 data = yf.download(ticker, start_date, end_date)
