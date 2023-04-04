0a611d3fbed6d18a6d14cc761151bb9ae8ad48c3
Josep Abellana <josep.abe.pu@gmail.com>
2023-04-04 21:45:11 +0200

    x

--- server/current_stock_data.py
+++ server/current_stock_data.py
@@ -5,3 +5,9 @@ previous_close_price = ticker['regularMarketPreviousClose']
 print('Ticker: GOOGL')
 print('Market Price:', market_price)
 print('Previous Close Price:', previous_close_price)
+
+
+
+import yfinance as yf
+ticker = yf.Ticker('GOOGL').info
+print(ticker.keys())
