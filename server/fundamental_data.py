eddd0e75e07b46df2752ae8ade48adc86ab6d7d4
Josep Abellana <josep.abe.pu@gmail.com>
2023-04-05 23:36:16 +0200

    x

--- server/fundamental_data.py
+++ server/fundamental_data.py
@@ -9,3 +9,5 @@ pb = msft.info['priceToBook']
 pe = msft.info['regularMarketPrice']/msft.info['trailingEps']
 print('Price to Book Ratio is: %.2f' % pb)
 print('Price to Earnings Ratio is: %.2f' % pe)
+
+
