e681021879797664214e6c69d0e130d92c6272e7
Josep Abellana <josep.abe.pu@gmail.com>
2023-04-05 23:36:07 +0200

    x

--- server/fundamental_data.py
+++ server/fundamental_data.py
@@ -3,3 +3,9 @@ import yfinance as yf
 
 # Set the ticker as MSFT
 msft = yf.Ticker("MSFT")
+
+# get price to book
+pb = msft.info['priceToBook']
+pe = msft.info['regularMarketPrice']/msft.info['trailingEps']
+print('Price to Book Ratio is: %.2f' % pb)
+print('Price to Earnings Ratio is: %.2f' % pe)
