80271da3fbd167c2f708a946a22309b0515bbe0a
Josep Abellana <josep.abe.pu@gmail.com>
2023-04-07 11:15:03 +0200

    x

--- server/futures_options.py
+++ server/futures_options.py
@@ -9,3 +9,13 @@ stock_fut = get_history(symbol="HDFC",
 stock_fut.head()
 
 
+import matplotlib.pyplot as plt
+stock_fut.Close.plot(figsize=(10, 5))
+# Define the label for the title of the figure
+plt.title("Close Price", fontsize=16)
+# Define the labels for x-axis and y-axis
+plt.ylabel('Price', fontsize=14)
+plt.xlabel('Date', fontsize=14)
+# Plot the grid lines
+plt.grid(which="major", color='k', linestyle='-.', linewidth=0.5)
+plt.show()
